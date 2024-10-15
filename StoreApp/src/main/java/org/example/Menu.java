5340ad328d9bc9e5173f1a991f73585b56022224
pontus <pontus-forsberg@live.se>
2024-10-15 12:24:09 +0200

    added Menu.java

---
+++ StoreApp/src/main/java/org/example/Menu.java
@@ -0,0 +1,9 @@
+Public class Menu(){
+
+	Private String message;
+	Private String eMessage; 
+
+	public Menu(String message, String eMessage){
+		this.message = messageM;
+		this.eMessage = eMessage; 
+}
