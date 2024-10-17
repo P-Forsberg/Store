4294d2d316f163e2797cb099ead9430b1eed2055
anton-jj <jappinen.anton@protonmail.com>
2024-10-17 10:33:26 +0200

    person command list

--- store/app/src/main/java/org/example/person/Person.java
+++ store/app/src/main/java/org/example/person/Person.java
@@ -1,11 +1,18 @@
 package org.example.person;
 
+import java.util.ArrayList;
+
 public class Person {
 
     public String name;
+    private ArrayList<Command> commands;
 
     public Person(String name) {
         this.name = name;
+    }
+
+    public ArrayList<Command> getCommands() {
+        return commands;
     }
 
     public String getName() {
